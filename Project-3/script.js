cf8eb87a969542573a13e3b76fa9340709981010
Al Amin Hossain <105510011+Al-Amin-Hossain-32@users.noreply.github.com>
2022-09-19 07:44:00 +0600

    Add files via upload

---
+++ Project-3/script.js
@@ -0,0 +1,45 @@
+// Project Requirements:
+// Change the background color by generating random hex color by clicking a button
+
+//Steps 
+
+// step 1 : create onload handler
+window.onload =()=>{
+    main();
+}
+function main(){
+    const root = document.getElementById("root");
+    const btn = document.getElementById("change-btn");
+    const output = document.getElementById("output");
+    const copyBtn = document.getElementById("copy");
+
+
+    btn.addEventListener("click",function(){
+        const bgColor= generateHexColor();
+        root.style.backgroundColor = bgColor;
+        output.value = bgColor;
+    })
+    
+    copyBtn.addEventListener("click" , function(){
+        navigator.clipboard.writeText(output.value)
+        copyBtn.title = `${output.value} are copied`
+    })
+
+}
+
+// step 2 : random color generator function
+
+function generateHexColor(){
+    const red = Math.floor(Math.random()*255);
+    const green = Math.floor(Math.random()*255);
+    const blue = Math.floor(Math.random()*255);
+
+    return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`
+}
+
+// step 3 collect all necessary references 
+
+
+//step 4 : handle the click event
+
+// step 5: handle the copy button click event
